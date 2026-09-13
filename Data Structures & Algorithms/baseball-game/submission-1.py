95701ae4b9bee4b234e1af13e000d0fc5dd4dcff
Stefan Small <58984159+Sbdsmall@users.noreply.github.com>
2026-09-13 12:02:58 -0400

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        """
+            score = [1, 2]
+            score = [1, 2, 3] + 
+            score = [1, 2, ] C
+            score = [1, 2, 5] 
+            score = [1, 2, 5, 10] D
+            score = reduce([1, 2, 5, 10]) = 18
+        """
+        scores = []
+        scoresLen = 0
+        for myOp in operations:
+            print("scores: ", scores)
+            if myOp == "+":
+                x, y = scores[scoresLen - 2], scores[scoresLen - 1]#out of bounds
+                scores.append(x + y)
+                scoresLen += 1
+                continue
+            elif myOp == "C":
+                scores.pop()
+                scoresLen -= 1
+                continue
+            elif myOp == "D":
+                val = scores[scoresLen - 1] * 2 
+                scores.append(val)
+                scoresLen += 1
+                continue
+            else: 
+                scores.append(int(myOp))
+                scoresLen += 1
+        score = 0
+        for num in scores:
+            score += num
+        
+        return score
